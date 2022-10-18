8dc74ddc11655e687cfef96e771df3e92e1e34ea
Farid Amimri <farid.amimri@gmail.com>
2022-10-18 17:55:15 +0100

    Add FoodList component

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import Head from 'next/head'
-import Image from 'next/image'
+import FoodList from '../components/FoodList'
 import Slider from '../components/Slider'
 import styles from '../styles/Home.module.css'
 
@@ -12,6 +12,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Slider />
+      <FoodList />
     </div>
   )
 }
