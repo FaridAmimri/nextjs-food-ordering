cdb4c3cd77a66037972b7ff02d545650b958cce8
Farid Amimri <farid.amimri@gmail.com>
2022-10-23 21:56:23 +0100

    Add Product and Order Schema

---
+++ models/Product.js
@@ -0,0 +1,38 @@
+/** @format */
+
+import mongoose from 'mongoose'
+
+const ProductSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      maxlength: 60
+    },
+    description: {
+      type: String,
+      required: true,
+      maxlength: 200
+    },
+    image: {
+      type: String,
+      required: true
+    },
+    prices: {
+      type: [Number],
+      required: true
+    },
+    extraOptions: {
+      type: [
+        {
+          text: { type: String, required: true },
+          price: { type: Number, required: true }
+        }
+      ]
+    }
+  },
+  { timestamps: true }
+)
+
+export default mongoose.models.Product ||
+  mongoose.model('Product', ProductSchema)
