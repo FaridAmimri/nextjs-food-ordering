bb382657f70bd74e3e0439d8d00afeec30352811
Farid Amimri <farid.amimri@gmail.com>
2022-10-26 12:55:54 +0100

    Fetch API endpoint product id + handle price, size, extras and quantity

--- pages/product/[id].jsx
+++ pages/product/[id].jsx
@@ -4,101 +4,122 @@ import Image from 'next/image'
 import { useState } from 'react'
 import styles from '../../styles/Product.module.css'
 import { Checkbox, Input, Button } from '@nextui-org/react'
+import axios from 'axios'
 
-const pizza = {
-  id: 1,
-  image: '/assets/pizza.png',
-  name: 'MARGHERITA',
-  price: [19.95, 23.95, 27.95],
-  description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.'
+export const getServerSideProps = async ({ params }) => {
+  const res = await axios.get(`http://localhost:3000/api/products/${params.id}`)
+  return {
+    props: {
+      pizza: res.data
+    }
+  }
 }
 
-function Product() {
+function Product({ pizza }) {
+  const [price, setPrice] = useState(pizza.prices[0])
   const [size, setSize] = useState(0)
+  const [extras, setExtras] = useState([])
+  const [quantity, setQuantity] = useState(1)
 
+  const changePrice = (number) => {
+    setPrice(price + number)
+  }
+
+  const handleSize = (sizeIndex) => {
+    const difference = pizza.prices[sizeIndex] - pizza.prices[size]
+    setSize(sizeIndex)
+    changePrice(difference)
+  }
+
+  const handleChange = (event, option) => {
+    const checked = event
+
+    if (checked) {
+      changePrice(option.price)
+      setExtras([...extras, option])
+    } else {
+      changePrice(-option.price)
+      setExtras(extras.filter((extra) => extra._id !== option._id))
+    }
+  }
+  console.log(quantity)
   return (
     <div className={styles.container}>
       <div className={styles.productImg}>
         <div className={styles.imgContainer}>
-          <Image src={pizza.image} layout='fill' alt='' objectFit='contain' />
+          <Image
+            src={pizza.image}
+            layout='fill'
+            alt=''
+            objectFit='contain'
+            aria-label='pizza'
+            priority
+          />
         </div>
       </div>
       <div className={styles.productInfo}>
-        <h1 className={styles.title}>{pizza.name}</h1>
-        <span className={styles.price}>{pizza.price[size]} €</span>
-        <p className={styles.description}>
-          {pizza.description} Lorem ipsum dolor sit amet consectetur adipisicing
-          elit. Hic aut veniam rerum ullam? Culpa minus pariatur vero magnam
-          sint dicta accusantium fugiat. A, sed dicta sunt totam voluptatibus
-          atque delectus.
-        </p>
+        <h1 className={styles.title}>{pizza.title}</h1>
+        <span className={styles.price}>{price} €</span>
+        <p className={styles.description}>{pizza.description}</p>
         <h3 className={styles.choose}>Choose the size</h3>
         <div className={styles.sizes}>
-          <div className={styles.size} onClick={() => setSize(0)}>
-            <Image src='/assets/size.png' layout='fill' alt='' />
+          <div className={styles.size} onClick={() => handleSize(0)}>
+            <Image
+              src='/assets/size.png'
+              layout='fill'
+              alt=''
+              aria-label='pizza size Small'
+            />
             <span className={styles.text}>Small</span>
           </div>
-          <div className={styles.size} onClick={() => setSize(1)}>
-            <Image src='/assets/size.png' layout='fill' alt='' />
+          <div className={styles.size} onClick={() => handleSize(1)}>
+            <Image
+              src='/assets/size.png'
+              layout='fill'
+              alt=''
+              aria-label='pizza size Medium'
+            />
             <span className={styles.text}>Medium</span>
           </div>
-          <div className={styles.size} onClick={() => setSize(2)}>
-            <Image src='/assets/size.png' layout='fill' alt='' />
+          <div className={styles.size} onClick={() => handleSize(2)}>
+            <Image
+              src='/assets/size.png'
+              layout='fill'
+              alt=''
+              aria-label='pizza size Large'
+            />
             <span className={styles.text}>Large</span>
           </div>
         </div>
         <h3 className={styles.choose}>Choose additional ingredients</h3>
         <div className={styles.ingredients}>
-          <div className={styles.option}>
-            <Checkbox
-              color='warning'
-              id='double'
-              name='double'
-              size='xs'
-              className={styles.checkbox}
-              label='Double Ingredients'
-            />
-          </div>
-          <div className={styles.option}>
-            <Checkbox
-              color='warning'
-              id='cheese'
-              name='cheese'
-              size='xs'
-              className={styles.checkbox}
-              label='Extra Cheese'
-            />
-          </div>
-          <div className={styles.option}>
-            <Checkbox
-              color='warning'
-              id='spicy'
-              name='spicy'
-              size='xs'
-              className={styles.checkbox}
-              label='Spicy Sauce'
-            />
-          </div>
-          <div className={styles.option}>
-            <Checkbox
-              color='warning'
-              id='garlic'
-              name='garlic'
-              size='xs'
-              className={styles.checkbox}
-              label='Garlic Sauce'
-            />
-          </div>
+          {pizza.extraOptions.map((option) => (
+            <div className={styles.option} key={option._id}>
+              <Checkbox
+                color='warning'
+                id={option.text}
+                name={option.text}
+                size='md'
+                className={styles.checkbox}
+                label={option.text}
+                isRounded={true}
+                onChange={(event) => handleChange(event, option)}
+              />
+            </div>
+          ))}
         </div>
         <div className={styles.add}>
           <Input
             type='number'
-            value={1}
-            width='50px'
+            value={quantity}
+            width='70px'
             className={styles.quantity}
+            aria-label='input number'
+            id='undefined'
+            onChange={(event) => setQuantity(event.target.value)}
           />
 
-          <Button className={styles.button} color='warning' >
+          <Button className={styles.button} color='warning'>
             Add to Cart
           </Button>
         </div>
