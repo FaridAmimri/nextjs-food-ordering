0ff69a2bbfaf14a4095eb317c4c26532ed3a8cfb
Farid Amimri <farid.amimri@gmail.com>
2022-11-01 21:37:05 +0100

    Add req for API to get orders and post an order

---
+++ pages/api/orders/[id].js
@@ -0,0 +1,26 @@
+/** @format */
+
+import dbConnect from '../../../util/mongo'
+import Order from '../../../models/Order'
+
+export default async function handler(req, res) {
+  const {
+    method,
+    query: { id }
+  } = req
+
+  await dbConnect()
+
+  if (method === 'GET') {
+    try {
+      const order = await Order.findById(id)
+      res.status(200).json(order)
+    } catch (error) {
+      res.status(500).json(error)
+    }
+  }
+  if (method === 'PUT') {
+  }
+  if (method === 'DELETE') {
+  }
+}
---
+++ pages/api/orders/index.js
@@ -0,0 +1,31 @@
+/** @format */
+
+import dbConnect from '../../../util/mongo'
+import Order from '../../../models/Order'
+
+export default async function handler(req, res) {
+  const {
+    method,
+    query: { id }
+  } = req
+
+  await dbConnect()
+
+  if (method === 'GET') {
+    try {
+      const orders = await Order.find()
+      res.status(200).json(orders)
+    } catch (error) {
+      res.status(500).json(error)
+    }
+  }
+
+  if (method === 'POST') {
+    try {
+      const order = await Order.create(req.body)
+      res.status(201).json(order)
+    } catch (error) {
+      res.status(500).json(error)
+    }
+  }
+}
