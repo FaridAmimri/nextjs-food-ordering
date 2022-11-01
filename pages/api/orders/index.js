356e8fe8bfb9c24c207819f1427b957960da2bab
Farid Amimri <farid.amimri@gmail.com>
2022-11-01 21:38:38 +0100

    Update method req

--- pages/api/orders/index.js
+++ pages/api/orders/index.js
@@ -4,10 +4,7 @@ import dbConnect from '../../../util/mongo'
 import Order from '../../../models/Order'
 
 export default async function handler(req, res) {
-  const {
-    method,
-    query: { id }
-  } = req
+  const { method } = req
 
   await dbConnect()
 
