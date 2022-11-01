4065f381cc8e65b4dff9a1e0b048b599d90add0e
Farid Amimri <farid.amimri@gmail.com>
2022-11-01 21:15:15 +0100

    Update method API

--- pages/api/products/[id].js
+++ pages/api/products/[id].js
@@ -20,20 +20,8 @@ export default async function handler(req, res) {
   }
 
   if (method === 'PUT') {
-    try {
-      const product = await Product.create(req.body)
-      res.status(201).json(product)
-    } catch (error) {
-      res.status(500).json(error)
-    }
   }
 
   if (method === 'DELETE') {
-    try {
-      const product = await Product.create(req.body)
-      res.status(201).json(product)
-    } catch (error) {
-      res.status(500).json(error)
-    }
   }
 }
