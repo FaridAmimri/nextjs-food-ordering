724ccc022a00a898d98f383b11c460fb8938e5d5
Farid Amimri <farid.amimri@gmail.com>
2022-11-03 10:10:18 +0100

    Add PUT method API for updating order status

--- pages/api/orders/[id].js
+++ pages/api/orders/[id].js
@@ -19,8 +19,18 @@ export default async function handler(req, res) {
       res.status(500).json(error)
     }
   }
+
   if (method === 'PUT') {
+    try {
+      const order = await Order.findByIdAndUpdate(id, req.body, {
+        new: true
+      })
+      res.status(200).json(order)
+    } catch (error) {
+      res.status(500).json(error)
+    }
   }
+
   if (method === 'DELETE') {
   }
 }
