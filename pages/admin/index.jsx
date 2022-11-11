10fc83365654d3d2297fbdaa55a794d0300c0d93
Farid Amimri <farid.amimri@gmail.com>
2022-11-11 12:08:23 +0100

    Add AddProduct component

--- pages/admin/index.jsx
+++ pages/admin/index.jsx
@@ -7,6 +7,7 @@ import styles from '../../styles/Admin.module.css'
 import axios from 'axios'
 import { useState } from 'react'
 import { useRouter } from 'next/router'
+import AddProduct from '../../components/AddProduct'
 
 function Index({ productsList, ordersList }) {
   const [products, setProducts] = useState(productsList)
@@ -44,94 +45,108 @@ function Index({ productsList, ordersList }) {
 
   return (
     <div className={styles.container}>
-      <div className={styles.signoutButton}>
-        Sign Out
-        <FaSignOutAlt onClick={handleLogOut} />
+      <div className={styles.buttonContainer}>
+        <div className={styles.addButton}>
+          <AddProduct />
+        </div>
+        <div className={styles.logoutButton}>
+          <Button
+            color='error'
+            auto
+            iconRight={<FaSignOutAlt />}
+            onPress={handleLogOut}
+          >
+            Log out
+          </Button>
+        </div>
       </div>
-      <div className={styles.item}>
-        <h1 className={styles.title}>Products</h1>
-        <table className={styles.table}>
-          <tbody>
-            <tr className={styles.trTitle}>
-              <th>Image</th>
-              <th>Id</th>
-              <th>Title</th>
-              <th>Price</th>
-              <th>Action</th>
-            </tr>
-          </tbody>
-          {products.map((product) => (
-            <tbody key={product._id}>
-              <tr className={styles.trData}>
-                <td>
-                  <Image
-                    src={product.image}
-                    alt=''
-                    width={50}
-                    height={50}
-                    objectFit='cover'
-                  ></Image>
-                </td>
-                <td>{product._id.slice(0, 5)}...</td>
-                <td>{product.title}</td>
-                <td>{product.prices[0]}</td>
-                <td className={styles.buttonContainer}>
-                  <Button auto bordered size='xs' color='secondary'>
-                    Edit
-                  </Button>
-                  <Button
-                    auto
-                    bordered
-                    size='xs'
-                    color='error'
-                    onPress={() => handleDelete(product._id)}
-                  >
-                    Delete
-                  </Button>
-                </td>
+
+      <div className={styles.tableContainer}>
+        <div className={styles.item}>
+          <h1 className={styles.title}>Products</h1>
+          <table className={styles.table}>
+            <tbody>
+              <tr className={styles.trTitle}>
+                <th>Image</th>
+                <th>Id</th>
+                <th>Title</th>
+                <th>Price</th>
+                <th>Action</th>
               </tr>
             </tbody>
-          ))}
-        </table>
-      </div>
-      <div className={styles.item}>
-        <h1 className={styles.title}>Orders</h1>
-        <table className={styles.table}>
-          <tbody>
-            <tr className={styles.trTitle}>
-              <th>Id</th>
-              <th>Customer</th>
-              <th>Total</th>
-              <th>Payment</th>
-              <th>Status</th>
-              <th>Action</th>
-            </tr>
-          </tbody>
-          {orders.map((order) => (
-            <tbody key={order._id}>
-              <tr className={styles.trData}>
-                <td>{order._id.slice(0, 5)}...</td>
-                <td>{order.customer}</td>
-                <td>{order.total} €</td>
-                <td>
-                  {order.method === 0 ? <span>cash</span> : <span>paid</span>}
-                </td>
-                <td>{status[order.status]}</td>
-                <td>
-                  <Button
-                    auto
-                    bordered
-                    size='xs'
-                    color='success'
-                    onPress={() => handleStatus(order._id)}
-                  >
-                    Next Stage
-                  </Button>
-                </td>
+            {products.map((product) => (
+              <tbody key={product._id}>
+                <tr className={styles.trData}>
+                  <td>
+                    <Image
+                      src={product.image}
+                      alt=''
+                      width={50}
+                      height={50}
+                      objectFit='cover'
+                    ></Image>
+                  </td>
+                  <td>{product._id.slice(0, 5)}...</td>
+                  <td>{product.title}</td>
+                  <td>{product.prices[0]}</td>
+                  <td className={styles.buttonContainer}>
+                    <Button auto bordered size='xs' color='secondary'>
+                      Edit
+                    </Button>
+                    <Button
+                      auto
+                      bordered
+                      size='xs'
+                      color='error'
+                      onPress={() => handleDelete(product._id)}
+                    >
+                      Delete
+                    </Button>
+                  </td>
+                </tr>
+              </tbody>
+            ))}
+          </table>
+        </div>
+        <div className={styles.item}>
+          <h1 className={styles.title}>Orders</h1>
+          <table className={styles.table}>
+            <tbody>
+              <tr className={styles.trTitle}>
+                <th>Id</th>
+                <th>Customer</th>
+                <th>Total</th>
+                <th>Payment</th>
+                <th>Status</th>
+                <th>Action</th>
               </tr>
             </tbody>
-          ))}
-        </table>
+            {orders.map((order) => (
+              <tbody key={order._id}>
+                <tr className={styles.trData}>
+                  <td>{order._id.slice(0, 5)}...</td>
+                  <td>{order.customer}</td>
+                  <td>{order.total} €</td>
+                  <td>
+                    {order.method === 0 ? <span>cash</span> : <span>paid</span>}
+                  </td>
+                  <td>{status[order.status]}</td>
+                  <td>
+                    <Button
+                      auto
+                      bordered
+                      size='xs'
+                      color='success'
+                      onPress={() => handleStatus(order._id)}
+                    >
+                      Next Stage
+                    </Button>
+                  </td>
+                </tr>
+              </tbody>
+            ))}
+          </table>
+        </div>
       </div>
     </div>
   )
