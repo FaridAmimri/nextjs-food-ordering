e801e1a259b8c31d67af02a9d12a8b4ccfb2a47b
Farid Amimri <farid.amimri@gmail.com>
2022-11-11 12:09:06 +0100

    Delete AddProduct component

--- pages/index.js
+++ pages/index.js
@@ -2,13 +2,11 @@
 
 import axios from 'axios'
 import Head from 'next/head'
-import { useState } from 'react'
-import AddProduct from '../components/AddProduct'
 import FoodList from '../components/FoodList'
 import Slider from '../components/Slider'
 import styles from '../styles/Home.module.css'
 
-export default function Home({ productList, admin }) {
+export default function Home({ productList }) {
   return (
     <div className={styles.container}>
       <Head>
@@ -17,25 +15,16 @@ export default function Home({ productList, admin }) {
         <link rel='icon' href='/favicon.ico' />
       </Head>
       <Slider />
-      {admin && <AddProduct />}
       <FoodList productList={productList} />
     </div>
   )
 }
 
-export const getServerSideProps = async (ctx) => {
-  const cookie = ctx.req?.cookies || ''
-  let admin = true
-
-  if (cookie.token !== process.env.TOKEN) {
-    admin = false
-  }
-
+export const getServerSideProps = async () => {
   const res = await axios.get('http://localhost:3000/api/products')
   return {
     props: {
-      productList: res.data,
-      admin
+      productList: res.data
     }
   }
 }
