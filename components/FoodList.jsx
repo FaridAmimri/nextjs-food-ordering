ea32a0c7c5e2a4728d1db3b7df747b3024971fa8
Farid Amimri <farid.amimri@gmail.com>
2022-11-14 09:49:37 +0100

    Update text description

--- components/FoodList.jsx
+++ components/FoodList.jsx
@@ -9,12 +9,7 @@ function FoodList({ productList }) {
       <h1 className={styles.title}>PIZZA DI MAMA</h1>
       <p className={styles.description}>
         Welcome to Pizza Di Mama ! <br />
-        Our pizzas are all homemade by Mama. She makes for you any pizza you
-        want. <br />
-        Do you want to eat delicious pizzas with the best taste and the best
-        quality ? <br />
-        Don&apos;t waste time and Get ones below ! We&apos;ll tell Mama about
-        your order.
+        Pizzas are all homemade with joy by Mama.
       </p>
       <div className={styles.wrapper}>
         {productList.map((food) => (
